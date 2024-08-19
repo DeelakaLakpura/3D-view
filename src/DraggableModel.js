e9d02c48e58c1242d9f0e828c0872e4b559d3eb4
Deelaka Lakpura <deelaka.lakpura94@gmail.com>
2024-08-19 17:08:19 +0530

    Update DraggableModel.js

--- src/DraggableModel.js
+++ src/DraggableModel.js
@@ -1,8 +1,8 @@
 import React, { useRef, useEffect } from 'react';
-import { useGLTF } from '@react-three/drei';
+import { useGLTF, TransformControls } from '@react-three/drei';
 
-const DraggableModel = ({ url, scale, rotation, isSelected, onLoad, onClick }) => {
-  const { scene, } = useGLTF(url, true); // Load the model
+const DraggableModel = ({ url, scale, rotation, onLoad, onClick, isSelected }) => {
+  const { scene } = useGLTF(url, true);
   const modelRef = useRef();
   const controlsRef = useRef();
 
@@ -13,20 +13,30 @@ const DraggableModel = ({ url, scale, rotation, isSelected, onLoad, onClick }) =
   }, [modelRef, controlsRef]);
 
   useEffect(() => {
-    if (onLoad) onLoad(); // Trigger onLoad callback
-  }, [onLoad]);
+    if (modelRef.current) {
+      modelRef.current.scale.set(...scale);
+      modelRef.current.rotation.y = rotation;
+    }
+  }, [scale, rotation]);
 
   return (
-    <primitive
-      ref={modelRef}
-      object={scene}
-      scale={scale}
-      rotation={[0, rotation, 0]} // Apply rotation
-      onClick={onClick}
-      onLoad={onLoad} // Ensure onLoad is triggered
-    >
-      {/* Ensure the textures are loaded */}
-    </primitive>
+    <>
+      <primitive
+        ref={modelRef}
+        object={scene}
+        scale={scale}
+        rotation={[0, rotation, 0]}
+        onClick={onClick}
+        onLoad={onLoad}
+      />
+      {isSelected && (
+        <TransformControls
+          ref={controlsRef}
+          object={modelRef.current}
+          mode="translate"
+        />
+      )}
+    </>
   );
 };
 
